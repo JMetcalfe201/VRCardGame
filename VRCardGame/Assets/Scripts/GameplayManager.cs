f67b389de239e75192a35739410bee9dea49aeaf
John Metcalfe <jmetcalfe201@gmail.com>
2015-11-07 13:34:02 -0600

    Added Basic Networking Functionality Added Arena art

---
+++ VRCardGame/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum EGamePhase
+{
+    DrawPhase,
+    MainPhase1,
+    BattlePhase,
+    MainPhase2,
+    EndPhase,
+}
+
+public class GameplayManager : NetworkBehaviour
+{
+    [SyncVar(hook = "OnCurrentPhaseChange")]
+    public EGamePhase currentPhase;
+
+    [SyncVar(hook = "OnFirstPlayersTurnChange")]
+    private bool firstPlayersTurn;
+
+    public Text phaseText;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPhase = EGamePhase.DrawPhase;
+        firstPlayersTurn = true;
+
+        phaseText.text = "It is player " + (firstPlayersTurn ? 1 : 2) + "'s turn. Phase: " + currentPhase.ToString();
+
+        NetworkManager.singleton.GetComponent<NetworkManagerHUD>().showGUI = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void AdvancePhase()
+    {
+        if(hasAuthority)
+        {
+            if(currentPhase < EGamePhase.EndPhase)
+            {
+                currentPhase++;
+            }
+            else
+            {
+                firstPlayersTurn = !firstPlayersTurn;
+                currentPhase = EGamePhase.DrawPhase;
+            }
+        }
+    }
+
+    // Handler for phase change
+    private void OnCurrentPhaseChange(EGamePhase newPhase)
+    {
+        currentPhase = newPhase;
+        UpdateUI();
+    }
+
+    private void OnFirstPlayersTurnChange(bool newTurn)
+    {
+        firstPlayersTurn = newTurn;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        phaseText.text = "It is player " + (firstPlayersTurn ? 1 : 2) + "'s turn. Phase: " + currentPhase.ToString();
+    }
+
+    public EGamePhase GetCurrentPhase()
+    {
+        return currentPhase;
+    }
+
+    public bool isPlayerOnesTurn()
+    {
+        return firstPlayersTurn;
+    }
+}
---
+++ VRCardGame/Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    private int lifepoints;
+
+    [SyncVar]
+    private bool isFirstPlayer;
+
+    public Vector2 cursorPosition;
+    public GameplayManager gpManager;
+
+    public Text playerText;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if(!isLocalPlayer)
+        {
+            GetComponent<AudioListener>().enabled = false;
+            GetComponent<Camera>().tag = "Untagged";
+            GetComponent<Camera>().enabled = false;
+        }
+
+        gpManager = GameObject.Find("GameplayManager").GetComponent<GameplayManager>();
+
+        isFirstPlayer = (NetworkManager.singleton.numPlayers == 1);
+
+        if (isLocalPlayer)
+        {
+            playerText = GameObject.Find("PlayerText").GetComponent<Text>();
+            playerText.text = "You are player: " + (isFirstPlayer ? 1 : 2);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isLocalPlayer)
+        {
+            HandleInput();
+        }
+	}
+
+    private void HandleInput()
+    {
+        if (!VRSettings.enabled)
+        {
+            float lookSensitivity = 5.0f;
+
+            transform.eulerAngles = transform.eulerAngles + new Vector3(0f, Input.GetAxis("Mouse X") * lookSensitivity, 0f);
+            transform.eulerAngles = transform.eulerAngles + new Vector3(-Input.GetAxis("Mouse Y") * lookSensitivity, 0f, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if ((gpManager.isPlayerOnesTurn() && isFirstPlayer) || (!gpManager.isPlayerOnesTurn() && !isFirstPlayer))
+            {
+                Debug.Log("Turn: " + gpManager.isPlayerOnesTurn() + "Player: " + isFirstPlayer);
+
+                CmdgpManagerAdvancePhase();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleNetworkGUI();
+        }
+    }
+
+    [Command]
+    private void CmdgpManagerAdvancePhase()
+    {
+        gpManager.AdvancePhase();
+    }
+
+    private void ToggleNetworkGUI()
+    {
+        NetworkManagerHUD hud = NetworkManager.singleton.GetComponent<NetworkManagerHUD>();
+        hud.showGUI = !hud.showGUI;
+    }
+
+    public void ChangeLifePoints(int points)
+    {
+        if(hasAuthority)
+        {
+            lifepoints += points;
+        }
+    }
+}
