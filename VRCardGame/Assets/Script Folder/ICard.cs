f3d7ffa5e7459fc471809900e307e936961c34e9
Jackson Montgomery <arcthelad56@yahoo.com>
2015-11-10 15:29:27 -0600

    Trying to push ICard
    Well, here goes!

---
+++ VRCardGame/Assets/Script Folder/ICard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ICard : MonoBehaviour
+{
+
+
+    string name;
+    string description;
+    ECardType cardtype;
+    GameObject _3Dmodel;
+    bool revealed;
+    event OnSummoned;
+    event OnToGraveyard;
+    event OnToHand;
+    event OnReveal;
+    event OnAttack;
+    event OnDefend;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
+enum ECardType
+{
+
+};
