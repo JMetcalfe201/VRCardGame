160b64afb8d70aa70bc2e096699d1d6d4231c4b7
John Metcalfe <jmetcalfe201@gmail.com>
2015-11-21 21:59:42 -0600

    Added placeholder UI and main menu

---
+++ VRCardGame/Assets/Scripts/PHMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PHMenuItem : MonoBehaviour
+{
+    public UnityEvent action;
+
+    public void Select()
+    {
+        action.Invoke();
+    }
+}
